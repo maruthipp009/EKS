a04f3d98822120616bde066d13508a8800112ffc
Maruthi Prasad <36444667+maruthipp009@users.noreply.github.com>
2021-10-05 19:58:01 +0530

    Update eks.tf
    2nd

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 # Provider
  provider "aws" {
-   access_key = "AKIAYPYDXJZ5YJY4F5XG"
-   secret_key = "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"
+   access_key = "AKIAYPYDXJZ5SSDU2V5Q"
+   secret_key = "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"
    region = "us-west-2"
  }
 resource "aws_iam_role" "eks_cluster" {
